5878227f3d28ac61a65d4541891935eb80284e07
jackill <jackillman@gmail.com>
2018-05-14 18:58:28 +0300

    d

--- app/js/common.js
+++ app/js/common.js
@@ -46,7 +46,21 @@ $(document).ready(function(){
 		
 		
 			slidesToShow: 2,
-			slidesToScroll: 1
+			
+			responsive: [
+				{
+				  breakpoint: 768,
+				  settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true
+				  }
+				}
+				// You can unslick at a given breakpoint now by adding:
+				// settings: "unslick"
+				// instead of a settings object
+			  ]
 		});
 		
 
