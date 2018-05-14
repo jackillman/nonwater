3106389e5205b4ce8d5f70d966796ccb4e28c065
jackill <jackillman@gmail.com>
2018-05-14 18:18:37 +0300

    non responsive

--- app/js/common.js
+++ app/js/common.js
@@ -32,4 +32,40 @@ $(document).ready(function(){
 		clock2.setCountdown(true);
 		clock2.start();
 
+		$('.review-slider').slick({
+			autoplay: true,
+			autoplaySpeed: 5000,
+			dots: true
+		});
+		$('.videos-slider').slick({
+			 autoplay: true,
+			autoplaySpeed: 5000,
+			dots: true
+		});
+		$('.wemedia-slider').slick({
+		
+		
+			slidesToShow: 2,
+			slidesToScroll: 1
+		});
+		
+
+		$('.video').click(function(){this.paused?this.play():this.pause();});
+    
+		// $(function () {
+		// 	var icons = {
+		// 		header: "circle-plus1",
+		// 		activeHeader: "circle-minus1"
+		// 	};
+		// 	$("#accordion").accordion({
+		// 		icons: icons
+		// 	});
+		// 	$("#toggle").button().on("click", function () {
+		// 		if ($("#accordion").accordion("option", "icons")) {
+		// 			$("#accordion").accordion("option", "icons", null);
+		// 		} else {
+		// 			$("#accordion").accordion("option", "icons", icons);
+		// 		}
+		// 	});
+		// });
 })
